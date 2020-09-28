2cf6a535f34f0a7301f716690638a3d9b2ef17d3
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-09-29 03:42:41 +0900

    Click-drag now creates an element

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import * as React from "react"
 })()
 
 export default function App() {
+	const pointerDownRef = React.useRef(false)
+
+	// const [state, setAppState] = React.useState({
+	// 	x: 0,
+	// 	y: 0,
+	// })
+
 	const [x, setX] = React.useState(0)
 	const [y, setY] = React.useState(0)
 
@@ -13,11 +20,26 @@ export default function App() {
 
 			<div
 				className="bg-blue-100 min-h-screen"
+				onPointerDown={() => {
+					pointerDownRef.current = true
+				}}
+				onPointerUp={() => {
+					pointerDownRef.current = false
+				}}
 				onPointerMove={e => {
+					if (!pointerDownRef.current) {
+						// No-op
+						return
+					}
 					setX(e.clientX)
 					setY(e.clientY)
 				}}
-			/>
+			>
+				<div
+					className="bg-blue-200"
+					style={{ height: y }}
+				/>
+			</div>
 
 			{/* Debug */}
 			<div className="p-2 fixed bottom-0 right-0">
