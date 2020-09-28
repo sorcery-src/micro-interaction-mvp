b886e8f386c92dd7d91b4284e96d09cd2f91f00e
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-09-29 04:09:17 +0900

    Changed to a data structure and added update instead of setState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import * as React from "react"
 
-;(() => {
-	console.clear()
-})()
+function useLayoutClear() {
+	React.useLayoutEffect(() => {
+		setTimeout(() => {
+			console.clear()
+		}, 1e3)
+	}, [])
+}
 
+/* eslint-disable */
 export default function App() {
-	const pointerDownRef = React.useRef(false)
+	useLayoutClear()
 
-	// const [state, setAppState] = React.useState({
-	// 	x: 0,
-	// 	y: 0,
-	// })
+	const [state, setState] = React.useState({
+		pointerCoords: {
+			x: 0,
+			y: 0,
+		},
+		pointerDown: false,
+		elements: [],
+	})
 
-	const [x, setX] = React.useState(0)
-	const [y, setY] = React.useState(0)
+	const update = React.useMemo(() => {
+		return replace => setState(current => ({
+			...current,
+			...replace,
+		}))
+	}, [setState])
 
 	return (
 		<>
@@ -21,39 +34,37 @@ export default function App() {
 			<div
 				className="bg-blue-100 min-h-screen"
 				onPointerDown={e => {
-					pointerDownRef.current = true
-					setX(Math.round(e.clientX))
-					setY(Math.round(e.clientY))
+					update({ pointerDown: true })
 				}}
 				onPointerUp={() => {
-					pointerDownRef.current = false
+					update({ pointerDown: false })
 				}}
 				onPointerMove={e => {
-					if (!pointerDownRef.current) {
-						// No-op
-						return
-					}
-					setX(Math.round(e.clientX))
-					setY(Math.round(e.clientY))
+					update({
+						x: Math.round(e.clientX),
+						y: Math.round(e.clientY),
+					})
 				}}
 			>
 				<div
 					className="bg-blue-200"
-					style={{ height: y }}
+					// style={{ height: y }}
 				>
 
 					<div className="relative h-full">
 						<div className="p-2 absolute bottom-0 right-0">
-							({x}, {y})
+							<p className="font-mono text-sm tabular-nums">
+								{/* ({x}, {y}) */}
+							</p>
 						</div>
 					</div>
 				</div>
 			</div>
 
 			{/* Debug */}
-			<div className="p-2 fixed bottom-0 right-0">
-				<p className="font-mono text-sm tabular-nums">
-					({x}, {y})
+			<div className="p-2 fixed bottom-0 left-0">
+				<p className="whitespace-pre font-mono text-sm tabular-nums">
+					{JSON.stringify(state, null, 2)}
 				</p>
 			</div>
 
