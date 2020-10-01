55780a8364de1e6fb8591763dc715e143ec7fb1e
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-01 22:21:56 +0900

    Upgraded <Style>

--- src/Style.js
+++ src/Style.js
@@ -1,9 +1,15 @@
 // <Style> renders a CSS string once.
-function Style({ id, children: css }) {
+function Style({ id: uuid, children: css }) {
+	// Guards:
+	if (typeof id !== "string" || id === "") {
+		const errorMessage = "<Style>: Did you forget an ID prop? Try <Style id={<string>}>."
+		throw new Error(errorMessage)
+	}
+
 	// Prevent rerender:
 	if (document.getElementById(id)) {
 		return null
 	}
 	// Render:
-	return <style id={id}>{css}</style>
+	return <style id={uuid}>{css}</style>
 }
