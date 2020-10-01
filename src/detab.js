85ff7a3f4639ef89fc215e62d49ea77de9ac41a7
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-10-02 00:11:46 +0900

    Upgraded detab

--- src/detab.js
+++ src/detab.js
@@ -2,13 +2,11 @@
 //
 // Ex:
 //
-// const str = (
-//   `
-//     #foo {
-//       bar: baz;
-//     }
-//   `
-// )
+// const str = `
+//   #foo {
+//     bar: baz;
+//   }
+// `
 // detab(str) // "#foo {\n\tbar: baz;\n}"
 //
 export default function detab(str) {
@@ -21,20 +19,18 @@ export default function detab(str) {
 	if (str2.length > 0 && str[str.length - 1] === "\n") {
 		str = str2.slice(0, -1)
 	}
-	// ^\t+
+	// Iterate to non-tab:
 	let tabCount = 0
 	for (let x = 0; x < str2.length; x++) {
-		// Guard:
 		if (str2[x] !== "\t") {
+			// No-op
 			break
 		}
 		tabCount++
 	}
 	if (tabCount > 0) {
-		str2 = str2
-			.split("\n")
-			.map(each => each.slice(tabCount))
-			.join("\n")
+		// prettier-ignore
+		str2 = str2.split("\n").map(each => each.slice(tabCount)).join("\n")
 	}
 	return str2
 }
